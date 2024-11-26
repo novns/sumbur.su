867494f5c37d997f0b85feb1b9ec04cf90a9e448
Nick Novikov <nick.novikov@gmail.com>
2024-11-26 15:56:44 +0800

    main: Web service

--- sumbur/main.go
+++ sumbur/main.go
@@ -1,4 +1,17 @@
 package main
 
+import "github.com/savsgio/atreugo/v11"
+
 func main() {
+	server := atreugo.New(atreugo.Config{Addr: "127.0.0.1:8000"})
+
+	server.GET("/", func(ctx *atreugo.RequestCtx) error {
+		return ctx.HTTPResponse("sumbur.su")
+	})
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
 }
